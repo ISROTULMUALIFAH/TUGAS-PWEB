36b847ac653ba128c1dbd9221a18c497849f56e8
Isrotul mualifah <88474551+ISROTULMUALIFAH@users.noreply.github.com>
2021-08-09 20:18:31 +0700

    Update db_20_Isrotulmualifah.sql

--- db_20_Isrotulmualifah.sql
+++ db_20_Isrotulmualifah.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 05, 2021 at 05:58 AM
--- Server version: 10.4.14-MariaDB
--- PHP Version: 7.2.33
+-- Generation Time: Aug 09, 2021 at 20:14 PM
+-- Server version: 10.4.20-MariaDB
+-- PHP Version: 7.4.21
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -28,17 +28,17 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `tb_siswa` (
-  `id_siswa` int(11) NOT NULL,
+  `id_siswa` int(10) NOT NULL,
   `nama` varchar(100) NOT NULL,
-  `hp` int(20) NOT NULL
+  `no_hp` int(14) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `tb_siswa`
 --
 
-INSERT INTO `tb_siswa` (`id_siswa`, `nama`, `hp`) VALUES
-(1, 'isrotulmualifah', 140405);
+INSERT INTO `tb_siswa` (`id_siswa`, `nama`, `no_hp`) VALUES
+(1, 'Isrotul mualifah', 2021)
 
 --
 -- Indexes for dumped tables
@@ -58,9 +58,10 @@ ALTER TABLE `tb_siswa`
 -- AUTO_INCREMENT for table `tb_siswa`
 --
 ALTER TABLE `tb_siswa`
-  MODIFY `id_siswa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_siswa` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+© 2021 GitHub, Inc.
